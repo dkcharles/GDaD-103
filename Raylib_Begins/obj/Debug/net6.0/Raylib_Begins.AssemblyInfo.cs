10df4cba60827dca5cffbe96ba6c8894b143e796
Darryl Charles <dk.charles@ulster.ac.uk>
2024-10-10 12:48:37 +0100

    tweaked gamemanger so only one constructor

--- Raylib_Begins/obj/Debug/net6.0/Raylib_Begins.AssemblyInfo.cs
+++ Raylib_Begins/obj/Debug/net6.0/Raylib_Begins.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Raylib_Begins")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57fd3107131c2fcf0b1a84b8a6bed92b63563f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bb5a52eafe1331a43d3a2da9ee0f3390882def")]
 [assembly: System.Reflection.AssemblyProductAttribute("Raylib_Begins")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Raylib_Begins")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
